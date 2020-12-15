5f2ef6be7f57c36140c1641985a80f00c93878c9
Marco Monteiro <marcomonteiro94@hotmail.com>
2020-12-15 00:00:53 -0300

    Added modal style

--- src/components/PokemonModal/PokemonModal.js
+++ src/components/PokemonModal/PokemonModal.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { ContainerModal, WrapperModal } from "./styles";
+import {
+  ContainerModal,
+  WrapperModal,
+  WrapperProfile,
+  BackgroundCircle,
+  AbsoluteDiv,
+  DescriptionWrapper,
+  BiologyDiv,
+  Button,
+  ButtonWrapper,
+} from "./styles";
 
 const PokemonModal = ({ pokemon, setModal }) => {
   const [pokemonModal, setPokemonModal] = React.useState(null);
@@ -25,15 +35,52 @@ const PokemonModal = ({ pokemon, setModal }) => {
     return (
       <ContainerModal onClick={handleOutsideClick}>
         <WrapperModal>
-          <h1>{pokemonModal.name}</h1>
-          <h2>{pokemonModal.types[0].type.name}</h2>
-          <img
-            src={pokemonModal.sprites.other["official-artwork"].front_default}
-            alt={pokemonModal.name}
-          />
-          <p>#{pokemonModal.id}</p>
-          <p>Altura: {(pokemonModal.height * 0.1).toFixed(2)}m</p>
-          <p>Peso: {(pokemonModal.weight * 0.1).toFixed(2)}kg</p>
+          <WrapperProfile>
+            <h1>{pokemonModal.name}</h1>
+            <div>
+              <div>
+                {pokemonModal.types.map((types) => (
+                  <h2 key={types.type.name}>{types.type.name}</h2>
+                ))}
+              </div>
+              <h2>#{pokemonModal.id}</h2>
+            </div>
+          </WrapperProfile>
+          <AbsoluteDiv>
+            <BackgroundCircle />
+            <img
+              src={pokemonModal.sprites.other["official-artwork"].front_default}
+              alt={pokemonModal.name}
+            />
+          </AbsoluteDiv>
+          <DescriptionWrapper>
+            <ButtonWrapper>
+              <Button>Sobre</Button>
+              <Button>Evolução</Button>
+              <Button>Status</Button>
+            </ButtonWrapper>
+            <div>
+              <h3>Descrição</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum ipsam enim asperiores magnam placeat animi quibusdam
+                sapiente similique modi rerum?
+              </p>
+            </div>
+            <div>
+              <h3>Biologia</h3>
+              <BiologyDiv>
+                <div>
+                  <h4>Altura</h4>
+                  <p>{(pokemonModal.height * 0.1).toFixed(2)}m</p>
+                </div>
+                <div>
+                  <h4>Peso</h4>
+                  <p>{(pokemonModal.weight * 0.1).toFixed(2)}kg</p>
+                </div>
+              </BiologyDiv>
+            </div>
+          </DescriptionWrapper>
         </WrapperModal>
       </ContainerModal>
     );
--- src/components/PokemonModal/styles.js
+++ src/components/PokemonModal/styles.js
@@ -12,10 +12,116 @@ export const ContainerModal = styled.div`
   align-items: center;
   z-index: 1000;
   padding: 2rem calc(4rem + 15px) 2rem 4rem;
+  text-align: justify;
 `;
 
 export const WrapperModal = styled.div`
-  width: 600px;
-  height: 800px;
+  width: 360px;
+  height: 600px;
   background: white;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-rows: 2fr 3.5fr;
+  position: relative;
+`;
+
+export const AbsoluteDiv = styled.div`
+  justify-self: center;
+  align-self: center;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 37%;
+
+  img {
+    position: absolute;
+    width: 150px;
+    z-index: 1000;
+  }
+`;
+
+export const BackgroundCircle = styled.div`
+  width: 140px;
+  height: 140px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+  justify-self: center;
+  align-self: center;
+  opacity: 0.5;
+  z-index: 1;
+`;
+
+export const WrapperProfile = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #7cc077;
+  padding: 1rem;
+  color: white;
+
+  h1 {
+    margin: 2rem 0 0 0;
+  }
+
+  h1,
+  h2 {
+    text-transform: capitalize;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div {
+      display: flex;
+      justify-content: space-space-between;
+      width: 50%;
+
+      h2 {
+        background: rgba(255, 255, 255, 0.3);
+        padding: 0.4rem;
+        border-radius: 1rem;
+        font-size: 1.4rem;
+      }
+    }
+  }
+`;
+
+export const DescriptionWrapper = styled.div`
+  margin: 5rem 2rem 0;
+`;
+
+export const BiologyDiv = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+
+    h4 {
+      margin: 0 1rem 0 0;
+      color: #aaa;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  background: transparent;
+  border: none;
+
+  &:hover,
+  &:focus {
+    color: #7cc077;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
